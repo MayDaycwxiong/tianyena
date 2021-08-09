6ad23ba68d6fbb5b5ec28865c951ae49c57f807d
cuiwx <cuiwx123>
2021-08-09 22:44:15 +0800

    tianyena-消息服务重新初始化

---
+++ message-service/message-service-deploy/src/main/java/com/tianyena/messageservicedeploy/MessageServiceDeployApplication.java
@@ -0,0 +1,13 @@
+package com.tianyena.messageservicedeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageServiceDeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageServiceDeployApplication.class, args);
+    }
+
+}
---
+++ message-service/message-service-deploy/src/test/java/com/tianyena/messageservicedeploy/MessageServiceDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tianyena.messageservicedeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageServiceDeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
