149534c0410c3a858005caf2ac06a86a24a09f03
MayDaycwxiong <1451780593@qq.com>
2021-08-23 01:21:22 +0800

    tianyena-账单服务-初始化

---
+++ bill-service/bill-service-deploy/src/main/java/com/tianyena/billservicedeploy/BillServiceDeployApplication.java
@@ -0,0 +1,13 @@
+package com.tianyena.billservicedeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillServiceDeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillServiceDeployApplication.class, args);
+    }
+
+}
---
+++ bill-service/bill-service-deploy/src/test/java/com/tianyena/billservicedeploy/BillServiceDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tianyena.billservicedeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillServiceDeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
