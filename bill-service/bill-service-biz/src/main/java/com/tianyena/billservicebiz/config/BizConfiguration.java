9ebea73095ab7e1b0c21852769c85dde5f7480a4
MayDaycwxiong <1451780593@qq.com>
2021-08-26 23:15:54 +0800

    tianyena-账单服务-具有web功能

--- bill-service/bill-service-biz/src/main/java/com/tianyena/billservicebiz/config/BizConfiguration.java
+++ bill-service/bill-service-biz/src/main/java/com/tianyena/billservicebiz/config/BizConfiguration.java
@@ -1,5 +1,6 @@
 package com.tianyena.billservicebiz.config;
 
+import com.tianyena.billservicemanager.config.ManagerConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
